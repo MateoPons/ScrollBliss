3182df7889e699c6f506faaf5b45d9956b634c19
Ready! <Ready@MacBook-Pro-de-Macintosh.local>
2023-02-14 11:33:41 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Scroll Bliss`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
